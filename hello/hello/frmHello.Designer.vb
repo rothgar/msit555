c5900515ad3fe94f8a0df8a09046198572819b08
Justin Garrison <justinleegarrison@gmail.com>
2014-09-12 23:07:26 -0700

    hello project v1

---
+++ hello/hello/frmHello.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmHello
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.radEnglish = New System.Windows.Forms.RadioButton()
+        Me.radItalian = New System.Windows.Forms.RadioButton()
+        Me.radFrench = New System.Windows.Forms.RadioButton()
+        Me.txtHello_World = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(139, 106)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(75, 23)
+        Me.btnOK.TabIndex = 1
+        Me.btnOK.Text = "&OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(32, 106)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'radEnglish
+        '
+        Me.radEnglish.AutoSize = True
+        Me.radEnglish.Checked = True
+        Me.radEnglish.Location = New System.Drawing.Point(32, 65)
+        Me.radEnglish.Name = "radEnglish"
+        Me.radEnglish.Size = New System.Drawing.Size(59, 17)
+        Me.radEnglish.TabIndex = 3
+        Me.radEnglish.TabStop = True
+        Me.radEnglish.Text = "English"
+        Me.radEnglish.UseVisualStyleBackColor = True
+        '
+        'radItalian
+        '
+        Me.radItalian.AutoSize = True
+        Me.radItalian.Location = New System.Drawing.Point(161, 65)
+        Me.radItalian.Name = "radItalian"
+        Me.radItalian.Size = New System.Drawing.Size(53, 17)
+        Me.radItalian.TabIndex = 5
+        Me.radItalian.TabStop = True
+        Me.radItalian.Text = "Italian"
+        Me.radItalian.UseVisualStyleBackColor = True
+        '
+        'radFrench
+        '
+        Me.radFrench.AutoSize = True
+        Me.radFrench.Location = New System.Drawing.Point(97, 65)
+        Me.radFrench.Name = "radFrench"
+        Me.radFrench.Size = New System.Drawing.Size(58, 17)
+        Me.radFrench.TabIndex = 4
+        Me.radFrench.TabStop = True
+        Me.radFrench.Text = "French"
+        Me.radFrench.UseVisualStyleBackColor = True
+        '
+        'txtHello_World
+        '
+        Me.txtHello_World.Location = New System.Drawing.Point(32, 22)
+        Me.txtHello_World.Name = "txtHello_World"
+        Me.txtHello_World.Size = New System.Drawing.Size(182, 20)
+        Me.txtHello_World.TabIndex = 6
+        Me.txtHello_World.TabStop = False
+        Me.txtHello_World.Text = "Hello, World!"
+        '
+        'frmHello
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(238, 162)
+        Me.Controls.Add(Me.txtHello_World)
+        Me.Controls.Add(Me.radFrench)
+        Me.Controls.Add(Me.radItalian)
+        Me.Controls.Add(Me.radEnglish)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnOK)
+        Me.Name = "frmHello"
+        Me.Text = "Hello, World!"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnOK As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents radEnglish As System.Windows.Forms.RadioButton
+    Friend WithEvents radItalian As System.Windows.Forms.RadioButton
+    Friend WithEvents radFrench As System.Windows.Forms.RadioButton
+    Friend WithEvents txtHello_World As System.Windows.Forms.TextBox
+
+End Class
---
+++ hello/hello/frmHello.vb
@@ -0,0 +1,29 @@
+﻿' Module:       frmHello.vb
+' Author:       Richard A. Eckhart
+' Organization: Azusa Pacific University
+' Application:  MSIT555 – Graphical User Interfaces, hello programming project
+' Prepared for: Dr. Richard Eckhart
+' Description:  Form module for the Hello World! application
+'
+' Initial Coding:    1-1-2013 - RAE
+' Revisions:
+
+Option Explicit On
+
+Public Class frmHello
+
+    Private Sub btnExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExit.Click
+        End
+    End Sub
+
+    Private Sub btnOK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOK.Click
+        If radEnglish.Checked Then
+            txtHello_World.Text = "Hello, world!"
+        ElseIf Me.radFrench.Checked = CBool(1) Then
+            Me.txtHello_World.Text = "Bonjour tout le monde!"
+        ElseIf radItalian.Checked = True Then
+            txtHello_World.Text = "Ciao, mondo!"
+        End If
+    End Sub
+
+End Class
